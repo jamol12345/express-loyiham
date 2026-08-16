69018e55d0f9cc3bbbad6f4847e96347a61cf1c1
Jamoliddin <jamoliddinisaboyev@gmail.com>
2026-08-16 15:47:20 +0500

    updated

--- functions/api.mjs
+++ functions/api.mjs
@@ -7,12 +7,19 @@ const app = express();
 const __dirname = path.resolve()
 
 const onlyAdmin = []
+let movies = []
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res)=>{
     res.sendFile(__dirname + "/dist/index.html")
+})
+app.get("/home", (req, res)=>{
+    res.sendFile(__dirname + "/dist/home.html")
+})
+app.get("/admin", (req, res)=>{
+    res.sendFile(__dirname + "/dist/admin.html")
 })
 app.get("/users", (req, res)=>{
     res.send(onlyAdmin)
@@ -22,10 +29,33 @@ app.post("/sign-up", (req, res)=>{
     const {username, password} = req.body;
     const admin = {username, password};
     if(admin.username === "admin" && admin.password === "admin123") {
-        res.send("Xush kebsiz admin!")
+        res.redirect("/admin")
     }else {
         res.send("Siz kimsiz?")
     }
+})
+
+// Barcha kinolarni beradi
+app.get("/movies", (req, res)=>{
+    res.send(movies)
+})
+
+// Yangi kino qo'shadi
+app.post("/add-movie", (req, res)=>{
+    const {id, title, subtitle, image} = req.body;
+    const movie = {id, title, subtitle, image};
+    movies.push(movie);
+    res.redirect("/admin")
+})
+
+
+
+
+// ID bo'yicha kinoni o'chiradi
+app.post("/delete-movie", (req, res)=>{
+    const {id} = req.body;
+    movies = movies.filter(movie => movie.id !== id);
+    res.redirect("/admin")
 })
 
 
